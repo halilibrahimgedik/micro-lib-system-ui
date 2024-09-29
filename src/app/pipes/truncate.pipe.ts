ba7c5d51974281bfd4a7565490e35b291878049a
halilibrahimgedik <halilgedik4234@gmail.com>
2024-09-29 22:58:11 +0300

    added truncate pipe

---
+++ src/app/pipes/truncate.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'truncate',
+  standalone: true
+})
+export class TruncatePipe implements PipeTransform {
+
+  transform(value: string, limit: number = 15): string {
+    if (!value) {
+      return '';
+    }
+
+    const words = value.split(' ');
+    if (words.length > limit) {
+      return words.slice(0, limit).join(' ') + '...';
+    }
+
+    return value;
+  }
+
+}
